ab940ef784325925fa9162c65b35ab8f612aa157
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-04 08:56:00 +0545

    Scrapper on the process

--- BBCNEWS/spiders/BBC_NEWS_SCRAPPER.py
+++ BBCNEWS/spiders/BBC_NEWS_SCRAPPER.py
@@ -8,5 +8,18 @@ class BbcNewsScrapperSpider(scrapy.Spider):
     start_urls = ['http://https://www.bbc.com/nepali/news/']
 
     def parse(self, response):
-        NewsLinks = response.xpath('//div[@class="eagle-item__body"]/a/@href').extract()
-        pass
+        NewsLinks = response.xpath("//div[@class='eagle-item faux-block-link']/div[@class='eagle-item__body']/a/@href").extract()
+
+        for link in NewsLinks:
+            link = 'https://www.bbc.com' + link
+            n = yield scrapy.Request(link,self.parse_article)
+
+    def parse_article(self,response):
+        NewsTitle = response.xpath('//div[@class="story-body"]/h1/text()').extract()[0]
+        NewsBody = response.xpath('//div[@class="story-body__inner"]/p/text()').extract()
+
+        FinalNews = ''.join(NewsBody)
+        yield{
+            "News Title":NewsTitle,
+            "News Article":FinalNews
+        }
