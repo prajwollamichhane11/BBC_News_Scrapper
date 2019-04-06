ee5d86af5793b097ef921ac8b0e2c4ca52fd5af3
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-06 23:49:57 +0545

    Preprocessing and Summarization to be done

--- BBCNEWS/spiders/BBC_NEWS_SCRAPPER.py
+++ BBCNEWS/spiders/BBC_NEWS_SCRAPPER.py
@@ -4,22 +4,29 @@ import scrapy
 
 class BbcNewsScrapperSpider(scrapy.Spider):
     name = 'BBC_NEWS_SCRAPPER'
-    allowed_domains = ['https://www.bbc.com/nepali/news']
-    start_urls = ['http://https://www.bbc.com/nepali/news/']
+    allowed_domains = ['www.bbc.com/nepali/news']
+    start_urls = ['https://www.bbc.com/nepali/news/']
+
 
     def parse(self, response):
         NewsLinks = response.xpath("//div[@class='eagle-item faux-block-link']/div[@class='eagle-item__body']/a/@href").extract()
 
+        NumberOfNews = len(NewsLinks)
+
+        for i in range(NumberOfNews):
+            NewsLinks[i] = 'https://www.bbc.com' + NewsLinks[i]
+
         for link in NewsLinks:
-            link = 'https://www.bbc.com' + link
-            n = yield scrapy.Request(link,self.parse_article)
+            yield scrapy.Request(link,self.parse_article, dont_filter=True)
+
+
 
     def parse_article(self,response):
-        NewsTitle = response.xpath('//div[@class="story-body"]/h1/text()').extract()[0]
-        NewsBody = response.xpath('//div[@class="story-body__inner"]/p/text()').extract()
+        title = response.xpath("//div[@class='column--primary']/div[@class='story-body']/h1/text()").extract()[0]
+        article = response.xpath("//div[@class='column--primary']/div[@class='story-body']/div[@class='story-body__inner']/p").extract()
 
-        FinalNews = ''.join(NewsBody)
+        article_final = ''.join(article)
         yield{
-            "News Title":NewsTitle,
-            "News Article":FinalNews
+            "News Title":title,
+            "News Article":article_final
         }
